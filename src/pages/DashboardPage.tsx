dff0f9d39786edcbca760a7be828d65105b0e070
shubham <shubham@192.168.1.5>
2026-03-10 08:05:29 +0530

    removed

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -77,12 +77,12 @@ const DashboardPage = () => {
           </div>
           
           <div className="flex items-center gap-3 w-full md:w-auto">
-            <button 
+            {/* <button 
               onClick={() => navigate('/admin')} 
               className="flex-1 md:flex-none px-5 py-2.5 bg-slate-50 text-slate-600 rounded-xl border border-slate-200 hover:bg-slate-100 hover:text-slate-800 transition-colors flex items-center justify-center gap-2 font-medium text-sm"
             >
               <ShieldCheck size={18} /> Admin
-            </button>
+            </button> */}
             <button 
               onClick={handleLogout} 
               className="flex-1 md:flex-none px-5 py-2.5 bg-red-50 text-red-600 rounded-xl border border-red-100 hover:bg-red-100 transition-colors flex items-center justify-center gap-2 font-medium text-sm"
